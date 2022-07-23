937e63093f63e954c85c3a5aa7c3def01960b899
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-07-22 17:01:34 -0700

    BadgeCheck icon

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,8 +1,9 @@
 import { createSlice} from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
 
 
 const initialState = {
-    cartItems: [],
+    cartItems: cartItems,
     isLoading: false,
     amount: 0,
     total: 0,
